a3bef0328938baa0dab4fa910bd08e2e6e99333d
AnkithaShettigar <ankitha.yermal@gmail.com>
2022-08-18 13:53:10 +0530

    added object

---
+++ delProperty.js
@@ -0,0 +1,3 @@
+function Check(obj1) {
+     return(delete obj1.rollno);
+}
---
+++ paraOrNot.js
@@ -0,0 +1,10 @@
+function Check(obj1) {
+    let obj=Object.keys(obj1);
+    if(obj.length===0){
+      return "false";
+    }else{
+      return"true";
+    }
+   }
+   
+   
